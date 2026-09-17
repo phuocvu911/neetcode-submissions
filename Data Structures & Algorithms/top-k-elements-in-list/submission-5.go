c3073e6dbff415a58f081b2cfc08b43194b81107
Hoang Phuoc Vu - Danny <107795987+phuocvu911@users.noreply.github.com>
2026-09-18 00:32:35 +0300

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.go
@@ -0,0 +1,28 @@
+func topKFrequent(nums []int, k int) []int {
+	hashTable := make(map[int]int)
+	for _, num := range nums {
+		hashTable[num]++
+	}
+
+	helper := make([][]int, len(nums)+1)
+	for key, val := range hashTable {
+		helper[val] = append(helper[val], key)
+	}
+
+	res := []int{}
+	for i := len(helper)-1; i > 0; i-- {
+		if helper[i] == nil {
+			continue
+		}
+		if len(res) == k {
+				break
+			}
+		for j := range len(helper[i]) {
+			res = append(res, helper[i][j])
+			if len(res) == k {
+				break
+			}
+		}
+	}
+	return res
+}
